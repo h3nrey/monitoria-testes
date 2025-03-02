02c13fcbd2f1f623609c2b2caabb4383f575369d
pedorooo <phln@cin.ufpe.br>
2025-03-01 23:21:31 -0300

    FEAT: adding a test to remove a animal by name

--- src/challenge/zoo.spec.ts
+++ src/challenge/zoo.spec.ts
@@ -14,5 +14,12 @@ describe("Zoo", () => {
     expect(zoo.getAllAnimals()).toContain(lion);
   });
 
+  test("should remove an animal by name", () => {
+    const lion = new Animal("Simba", "Lion", 5);
+    zoo.addAnimal(lion);
+    zoo.removeAnimal("Simba");
+    expect(zoo.getAllAnimals()).not.toContain(lion);
+  });
+
 
 });
